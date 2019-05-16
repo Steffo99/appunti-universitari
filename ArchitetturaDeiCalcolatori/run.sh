2c5fba4fb94ba1daaf3cfd543c3aa4b8a0894c2a
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-05-16 16:19:38 +0200

    Update stack

--- ArchitetturaDeiCalcolatori/run.sh
+++ ArchitetturaDeiCalcolatori/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "Cleaning phase"
+rm "$1.o"
+rm "$1.elf"
+echo "Compilation phase"
+riscv64-unknown-elf-as -g -o "$1.o" "$1.riscv"
+riscv64-unknown-elf-ld -o "$1.elf" "$1.o"
+echo "Debugging phase"
 xterm -e "qemu-riscv64 -g 1234 \"$1.elf\"" &
 xterm -e "riscv64-unknown-elf-gdb --ex=\"target remote localhost:1234\" \"$1.elf\"" &
-
-exit $?
+wait
