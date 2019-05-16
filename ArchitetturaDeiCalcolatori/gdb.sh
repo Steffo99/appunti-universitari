3ec84efa2a3a6071dababd36a04d1abe19e712ba
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-05-16 15:46:50 +0200

    Update tools

--- ArchitetturaDeiCalcolatori/gdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-riscv64-unknown-elf-gdb --ex="target remote localhost:1234"
-
-exit $?
--- ArchitetturaDeiCalcolatori/run.sh
+++ ArchitetturaDeiCalcolatori/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Run the program
-qemu-riscv64 -g 1234 "$1.elf"
+xterm -e "qemu-riscv64 -g 1234 \"$1.elf\"" &
+xterm -e "riscv64-unknown-elf-gdb --ex=\"target remote localhost:1234\" \"$1.elf\"" &
 
 exit $?
